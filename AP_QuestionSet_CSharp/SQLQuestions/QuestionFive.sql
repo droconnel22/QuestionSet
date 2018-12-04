ad05bc0c0e91ed5c0918813ae751f39657583a7a
droconnel22 <droconnel22@gmail.com>
2018-12-03 21:48:39 -0500

    Completed QuestionFive

---
+++ AP_QuestionSet_CSharp/SQLQuestions/QuestionFive.sql
@@ -0,0 +1,10 @@
+﻿SELECT
+UA.UserId,
+UD.FirstName + ' ' + UD.LastName as 'Name',
+Max(CAST(UA.Timestamp AS DATE))
+FROM dbo.UserActivity as UA
+INNER JOIN
+dbo.UserData as UD
+ON
+UA.UserId = UD.Id
+GROUP BY UA.UserId,UD.FirstName + ' ' + UD.LastName
---
+++ AP_QuestionSet_CSharp/SQLQuestions/UserData.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[UserData]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [FirstName] NVARCHAR(50) NOT NULL, 
+    [LastName] NVARCHAR(50) NOT NULL
+)
